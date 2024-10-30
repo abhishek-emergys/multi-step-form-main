e117d54c28772ad84f289d5ad8cfd4d9a8199d4a
abhishek-emergys <abhishek.nalawade@emergys.com>
2024-10-31 00:53:06 +0530

    Implementd input validations

--- index.js
+++ index.js
@@ -1,74 +1,112 @@
-var currentTab = 3; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+var currentTab = 0;
+showTab(currentTab);
 
 function showTab(n) {
-  // This function will display the specified tab of the form...
-  console.log(n);
-  
+    console.log("NeXT showTab");
+    
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
   if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
+    document.getElementById("back-btn").style.visibility = "hidden";
   } else {
-    document.getElementById("prevBtn").style.display = "inline";
+    document.getElementById("back-btn").style.visibility = "visible";
   }
-  if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+
+  if (n == 4) {
+    document.getElementById("btn").style.display = "none";
   }
-  //... and run a function that will display the correct step indicator:
+
   fixStepIndicator(n)
 }
 
 function nextPrev(n) {
-  // This function will figure out which tab to display
-  var x = document.getElementsByClassName("tab");
-  // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
-  // Hide the current tab:
-  x[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
-  // if you have reached the end of the form...
-  if (currentTab >= x.length) {
-    // ... the form gets submitted:
-    document.getElementById("regForm").submit();
-    return false;
-  }
-  // Otherwise, display the correct tab:
-  showTab(currentTab);
-}
 
-function validateForm() {
-  // This function deals with validation of the form fields
-  var x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false
-      valid = false;
-    }
-  }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid; // return the valid status
+    const inputsValid = checkInputs();
+    console.log("inputsValid ", inputsValid);
+    
+    
+    if(inputsValid) {
+
+        console.log("NeXT nextPrev", n);
+    
+      var x = document.getElementsByClassName("tab");
+      console.log(x[currentTab]);
+      
+      if (n == 0) return false;
+      x[currentTab].style.display = "none";
+      currentTab = currentTab + n;
+      if (currentTab >= x.length) {
+        document.getElementById("form").submit();
+        return false;
+      }
+      showTab(currentTab);
+    } else return
 }
 
 function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
   var i, x = document.getElementsByClassName("step");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
   }
-  //... and adds the "active" class on the current step:
   x[n].className += " active";
+}
+
+function checkInputs(){ 
+    let username = document.getElementById('username').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+
+    const regexUsername = new RegExp("^[a-zA-Z]");
+    const regexEmail = /@/;
+
+    const validUsername = regexUsername.test(username);
+    const validEmail = regexEmail.test(email);
+    const validPhone = phone.length === 10
+
+    if(validUsername !== true) {
+        if(username.length < 1) {
+            document.getElementById("name-error").style.display = "block";
+        } else {
+            document.getElementById("name-error").innerHTML = "Invalid username";
+            document.getElementById("name-error").style.display = "block";
+        }
+    } else {
+        document.getElementById("name-error").style.display = "none";
+    }
+
+    if(validEmail !== true) {
+        if(email.length < 1) {
+            document.getElementById("email-error").style.display = "block";
+        } else {
+            document.getElementById("email-error").innerHTML = "Invalid email";
+            document.getElementById("email-error").style.display = "block";
+        }
+    } else {
+        document.getElementById("email-error").style.display = "none";
+    }
+
+    if(validPhone !== true) {
+        if(phone.length < 1) {
+            document.getElementById("phone-error").style.display = "block";
+        } else {
+            document.getElementById("phone-error").innerHTML = "Enter 10 digits phone number.";
+            document.getElementById("phone-error").style.display = "block";
+        }
+    } else {
+        document.getElementById("phone-error").style.display = "none";
+    }
+
+    console.log("validUsername ", validUsername);
+
+    console.log("validEmail ", validEmail);
+    
+    const obj = {
+        username: username,
+        email: email,
+        phone: phone,
+    }
+    
+    console.log(obj);
+
+    return validUsername && validEmail && validPhone;
 }
